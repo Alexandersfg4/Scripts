2efa6bfb29c509897bb91db12441466d958b621e
Alexandersfg4 <59264285+Alexandersfg4@users.noreply.github.com>
2020-12-16 17:41:44 +0300

    Add files via upload

---
+++ list_generator.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#the list generates list by the next varibles
+DEF_COUNRY_CODE=1
+AMOUNT_OF_RECORDS=500000
+#do not change the next part
+var=0
+cat <<'EOF' > calling_list$AMOUNT_OF_RECORDS.txt
+Account, Name, Phone number
+EOF
+until [ $var -eq $AMOUNT_OF_RECORDS ]
+do	
+	printf "$var, name, $DEF_COUNRY_CODE$var \n" >> calling_list$AMOUNT_OF_RECORDS.txt
+	var=`expr $var + 1`
+done 
+
