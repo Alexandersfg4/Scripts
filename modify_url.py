16a131237d65a3c097ac108db1f9d7ac8658da36
Alexandersfg4 <59264285+Alexandersfg4@users.noreply.github.com>
2021-06-04 18:24:55 +0300

    Update modify_url.py

--- modify_url.py
+++ modify_url.py
@@ -1,15 +1,11 @@
 from mitmproxy import http
 import re
 
-#query, value = "rating__gte", 4
-query, value = "rating", 0
+# Наши квери параметры
+query, value = "rating__gte", 4
+
 
 def request(flow: http.HTTPFlow) -> None:
     if re.match(r"https://myb11966.test.mybook.tech/api/books/*", flow.request.url):
         flow.request.query[query] = value
         flow.request.headers["Authorization"] = ''
-    elif re.match(r"https://myb11966.test.mybook.tech/api/catalog/series/\d*/books/", flow.request.url):
-        flow.request.query[query] = value
-        flow.request.headers["Authorization"] = ''
-    else:
-        pass
